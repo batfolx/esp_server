2a590f3541a69f6ebcd9600e1c739dc2c992736a
batfolx <victorvelea007@yahoo.com>
2020-10-04 14:30:10 -0400

    change name of file

--- serving.py
+++ serving.py
@@ -1,11 +1,20 @@
-
 import os
-
 from flask import Flask, render_template, request, jsonify, send_file
+from flask_socketio import SocketIO, send, emit
+
+pictures = {}
+connections = {}
 
 ip = '192.168.1.6'
 port = 3000
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@socketio.on('connected')
+def sock_connect():
+    print(f'We have a connection {request.namespace.socket.sessid}')
+    connections[request.namespace.socket] = 1
 
 
 @app.route("/")
@@ -16,21 +25,15 @@ def home():
 
 @app.route("/pic")
 def pics():
-
     try:
-
         fname = request.args['name']
-        return send_file(fname)
+        return pictures[int(fname)]
 
     except FileNotFoundError or ValueError or KeyError:
-        if not os.path.exists('cam1.jpg'):
+        if not os.path.exists('1.jpg'):
             return send_file('error.png')
         else:
-            return send_file('cam1.jpg')
-
-
-
-
+            return send_file('1.jpg')
 
 
 @app.route("/upload", methods=["POST"])
@@ -38,10 +41,18 @@ def upload():
     print(request.data)
     print(list(request.headers.keys()))
     print(request.headers['Content-Disposition'])
-    with open(request.headers['Content-Disposition'], 'wb') as file:
-        file.write(request.data)
-    return "OK!"
 
+    # filename will be like '1.jpeg'
+    filename = request.headers['Content-Disposition']
+    with open(filename, 'wb') as file:
+        file.write(request.data)
+
+    # get the number
+    number = int(filename.split('.')[0])
+
+    # store picture in memory
+    pictures[number] = request.data
+    return "OK!"
 
 
 if __name__ == '__main__':
