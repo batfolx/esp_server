23d72f5c7bb6d1af45a89c2f4fe4855a9b8457d0
batfolx <victorvelea007@yahoo.com>
2020-10-05 01:57:20 -0400

    added websocket for faster sending of picture

--- serving.py
+++ serving.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify, send_file, session, copy_current_request_context
 from flask_socketio import SocketIO, send, emit
+from threading import Thread
+import time
 
 pictures = {}
 connections = {}
@@ -8,13 +10,34 @@ connections = {}
 ip = '192.168.1.6'
 port = 3000
 app = Flask(__name__)
+
 socketio = SocketIO(app)
 
 
 @socketio.on('connected')
 def sock_connect():
-    print(f'We have a connection {request.namespace.socket.sessid}')
-    connections[request.namespace.socket] = 1
+    connections[session] = 1
+    print(f'We got a connection {connections}')
+
+
+@socketio.on('disconnect')
+def sock_disconnect():
+    try:
+        del connections[request.sid]
+    except Exception as e:
+        pass
+
+
+@socketio.on("camera")
+def handle_camera(data):
+    try:
+        camnum = int(data['cam'])
+    except Exception:
+        camnum = 1
+
+    emit('data', {
+        'data': pictures[camnum]
+    })
 
 
 @app.route("/")
@@ -38,9 +61,9 @@ def pics():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    print(request.data)
-    print(list(request.headers.keys()))
-    print(request.headers['Content-Disposition'])
+   # print(request.data)
+   # print(list(request.headers.keys()))
+   # print(request.headers['Content-Disposition'])
 
     # filename will be like '1.jpeg'
     filename = request.headers['Content-Disposition']
@@ -55,5 +78,8 @@ def upload():
     return "OK!"
 
 
+
+
 if __name__ == '__main__':
-    app.run(host=ip, port=port, debug=True)
+    socketio.run(app, host=ip, port=port, debug=True)
+
